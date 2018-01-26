433ee29cf4dc8243619693f607f3cfdf4c4b3e85
CarrotsGames <max.leslie.turner@gmail.com>
2018-01-27 01:38:34 +1100

    adding all of the scene and folders

---
+++ Game/Assets/Editor/SnapToNodeEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SnapToNode))]
+public class SnapToNodeEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        SnapToNode snapToNode = (SnapToNode)target;
+
+        if (GUILayout.Button("Snap"))
+            snapToNode.Snap();
+    }
+}
---
+++ Game/Assets/Scripts/InputController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+    public string transmitterTag;
+
+    private Camera mainCam;
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+    }
+
+	void Update () {
+        CheckLeftMouse();
+	}
+
+    private void CheckLeftMouse()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == transmitterTag)
+                    hit.transform.GetComponent<Transmitter>().HandleClick();
+            }
+        }
+    }
+}
