3fabd25860421a1a794461898f21041c0b3463bf
CarrotsGames <max.leslie.turner@gmail.com>
2018-01-27 13:05:56 +1100

    Scripts

---
+++ Scripts/Scripts/Scripts/Map/Node.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Node {
+    public Vector3 Position { get { return position;} }
+    public bool Occupied { get { return occupied; } }
+
+    private Vector3 position;
+    private bool occupied;
+
+    public Node(Vector3 position)
+    {
+        this.position = position;
+    }
+
+    public void UpdateOccupied(bool occupied)
+    {
+        this.occupied = occupied;
+    }
+}
---
+++ Scripts/Scripts/Scripts/Transmitters/SnapToNode.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapToNode : MonoBehaviour {
+    public static Map map;
+
+    private Node currentNode;
+
+    private void OnEnable()
+    {
+        if (map == null)
+            map = FindObjectOfType<Map>();
+    }
+
+    private bool PositionChanged()
+    {
+        return transform.position != currentNode.Position ? true : false;
+    }
+
+    public void Snap()
+    {
+        if (currentNode != null && transform.position == currentNode.Position)
+            return;
+
+        if(currentNode != null)
+            currentNode.UpdateOccupied(false);
+
+        Node node = map.GetNearestNode(transform.position);
+
+        if (node == null)
+        {
+            Debug.LogError(string.Format("{0} could not find a valid node to snap to", transform.name));
+            return;
+        }
+
+        currentNode = node;
+        currentNode.UpdateOccupied(true);
+
+        transform.position = currentNode.Position;
+    }
+
+    public void GiveNode(Node node)
+    {
+        currentNode = node;
+    }
+
+    private void OnDestroy()
+    {
+        currentNode.UpdateOccupied(false);
+    }
+}
